9e530d039c87a0830561d7226d6cf9681f24d984
bdao <chenghao900@163.com>
2018-12-21 10:28:36 +0800

    Update nagios_client.sh

--- install/nagios/nagios_client.sh
+++ install/nagios/nagios_client.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
+source ../header.sh
+#plugin_version=1.4.15
 plugin_version=2.2.1
 nrpe_version=3.1.0
-root=$PWD
-download=~/Downloads
-prefix=/usr/local/cellar
-sysctl_dir=/usr/lib/systemd/system/
+
+
 dependencies() {
-	yum install perl-devel perl-CPAN -y
+        yum install perl-devel perl-CPAN -y
 }
 
 download() {
@@ -17,17 +17,18 @@ download() {
 		#wget http://sourceforge.net/projects/nagiosplug/files/nagiosplug/$plugin_version/$plugin_tgz
 		tar zxvf $plugin_tgz 
 	fi
+
   nrpe_tgz=nrpe-$nrpe_version.tar.gz
-	if [ ! -f $download/$nrpe_tgz ];
+    if [ ! -f $download/$nrpe_tgz ];
 	then
-		#wget http://nchc.dl.sourceforge.net/project/nagios/nrpe-2.x/nrpe-$nrpe_version/$nrpe_tgz
+	#wget http://nchc.dl.sourceforge.net/project/nagios/nrpe-2.x/nrpe-$nrpe_version/$nrpe_tgz
 		wget https://sourceforge.net/projects/nagios/files/nrpe-3.x/$nrpe_tgz
 		tar zxvf $nrpe_tgz
 	fi
 }
 
 install() {
-	#plugin
+        #plugin
 	cd $download/nagios-plugins-$plugin_version
   ./configure --with-nagios-user=nagios --with-nagios-group=nagios --enable-perl-modules
 	make;make install
@@ -41,39 +42,29 @@ install() {
 	make install-plugin
   	make install-daemon
 	make install-daemon-config
-	
 }
 
 usergroup() {
-	useradd nagios -M -s /sbin/nologin
-	#passwd nagios
+        useradd nagios -M -s /sbin/nologin
+        #passwd nagios
 }
 
 config() {
-	mkdir /usr/local/nagios/etc/
-	cp $download/nrpe-$nrpe_version/sample-config/nrpe.cfg /usr/local/nagios/etc/nrpe.cfg
-	/usr/local/nagios/bin/nrpe -d -c /usr/local/nagios/etc/nrpe.cfg 
-	echo "/usr/local/nagios/bin/nrpe -d -c /usr/local/nagios/etc/nrpe.cfg" >> /etc/rc.local
-	/usr/local/nagios/libexec/check_nrpe -H localhost
-	cp $root/nrpe.cfg /usr/local/nagios/etc/nrpe.cfg
-	cp $root/check_mem.pl /usr/local/nagios/libexec/check_mem.pl
-	chmod 755 /usr/local/nagios/libexec/check_mem.pl
-	
+        mkdir /usr/local/nagios/etc/
+		cp $download/nrpe-$nrpe_version/sample-config/nrpe.cfg /usr/local/nagios/etc/nrpe.cfg
+		/usr/local/nagios/bin/nrpe -d -c /usr/local/nagios/etc/nrpe.cfg 
+		echo "/usr/local/nagios/bin/nrpe -d -c /usr/local/nagios/etc/nrpe.cfg" >> /etc/rc.local
+		/usr/local/nagios/libexec/check_nrpe -H localhost
+		cp $root/nrpe.cfg /usr/local/nagios/etc/nrpe.cfg
+		cp $root/check_mem.pl /usr/local/nagios/libexec/check_mem.pl
+		chmod 755 /usr/local/nagios/libexec/check_mem.pl
 }
 
 reload() {
-	kill -HUP `ps -ef|grep nrpe|awk 'NR==1{print $2}'`
-	/usr/local/nagios/libexec/check_nrpe -H localhost -c check_mem
-	/usr/local/nagios/libexec/check_nrpe -H localhost -c check_disk
-}
-
-main() {
-	dependencies
-	download
-	usergroup
-	install
-	config
+        kill -HUP `ps -ef|grep nrpe|awk 'NR==1{print $2}'`
+		/usr/local/nagios/libexec/check_nrpe -H localhost -c check_mem
+		/usr/local/nagios/libexec/check_nrpe -H localhost -c check_disk
 }
 
 
-main
+main $1
